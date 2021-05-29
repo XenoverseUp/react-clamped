7be855fb329f15eac55fca4ebe0825027822b0ee
XenoverseUp <xenoverseup@gmail.com>
2021-05-29 21:35:04 +0300

    Updates

---
+++ precache-manifest.ff8757bda6108897ff18cef6d0e898f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2c554f786385cc87201426d887486b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c7f90e77acc13a880f5",
+    "url": "./static/css/main.b1395ef6.chunk.css"
+  },
+  {
+    "revision": "b710d639466bd56ce631",
+    "url": "./static/js/2.8d1631fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8d1631fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7f90e77acc13a880f5",
+    "url": "./static/js/main.785989ac.chunk.js"
+  },
+  {
+    "revision": "8ac5846ff6005516a949",
+    "url": "./static/js/runtime-main.4180f169.js"
+  }
+]);
